489dc5e1823e83848b16b5a1a6bba2e63fc14a8b
Daniel Gil <tiverius37@gmail.com>
2023-10-09 16:01:39 -0500

    no re_path

--- my_portfolio_website/urls.py
+++ my_portfolio_website/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic.base import RedirectView
-
+#from django.views.generic.base import RedirectView
 
 urlpatterns = [
     #re_path(r'^favicon\.ico', RedirectView.as_view(url='\static\website\favicon.ico', permanent=True)),
